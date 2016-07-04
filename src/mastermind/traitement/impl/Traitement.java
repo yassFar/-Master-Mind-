fcf44fb46c7beb8f6ec7e9ddb13ec3c4abca0b1a
Kevin <kevin.basse@epsi.fr>
2016-07-04 22:36:20 +0200

    Création de la méthode traitement + REFACTO

--- src/mastermind/traitement/impl/Traitement.java
+++ src/mastermind/traitement/impl/Traitement.java
@@ -7,44 +7,10 @@ import mastermind.traitement.enums.CouleurEnum;
 import mastermind.traitement.enums.ResultatEnum;
 
 public class Traitement {
-    // public List<ResultatEnum> traitement(List<CouleurEnum> couleursBase,
-    // List<CouleurEnum> couleursProposees) {
-    // couleursBase.add(CouleurEnum.BLEU);
-    // couleursBase.add(CouleurEnum.FUSHIA);
-    // couleursBase.add(CouleurEnum.JAUNE);
-    // couleursBase.add(CouleurEnum.ORANGE);
-    //
-    // couleursProposees.add(CouleurEnum.ORANGE);
-    // couleursProposees.add(CouleurEnum.JAUNE);
-    // couleursProposees.add(CouleurEnum.VERT);
-    // couleursProposees.add(CouleurEnum.ORANGE);
-    //
-    // return resultats;
-    // }
-
-    // TODO : MAIN à changer en méthode
-    // Méthode traite le choix de l'utilisateur
-    // Paramètre 1 : une liste de 4 couleurs choisit par l'ordi ou le joueur dès
-    // le départ de la partie
-    // Paramètre 2: une liste de 4 couleurs proposées par le joueur à chaque
-    // coup
-    // return: liste avec 3 état possible:
     // MAUVAIS
     // MAL_PLACE_COULEUR_OK
     // BON
-    public static void main(String[] args) {
-        List<CouleurEnum> couleursBase = new ArrayList<>();
-        List<CouleurEnum> couleursProposees = new ArrayList<>();
-        couleursBase.add(CouleurEnum.BLEU);
-        couleursBase.add(CouleurEnum.FUSHIA);
-        couleursBase.add(CouleurEnum.JAUNE);
-        couleursBase.add(CouleurEnum.ORANGE);
-
-        couleursProposees.add(CouleurEnum.BLEU);
-        couleursProposees.add(CouleurEnum.JAUNE);
-        couleursProposees.add(CouleurEnum.ORANGE);
-        couleursProposees.add(CouleurEnum.ORANGE);
-
+    public List<ResultatEnum> traitement (List<CouleurEnum> couleursBase, List<CouleurEnum> couleursProposees) {
         List<ResultatEnum> resultats = new ArrayList<ResultatEnum>();
         for (int i = 0; i < 4; i++) {
             if (couleursProposees.get(i) == couleursBase.get(i)) {
@@ -55,9 +21,7 @@ public class Traitement {
                 resultats.add(i, ResultatEnum.MAUVAIS);
             }
         }
-        for (ResultatEnum res : resultats) {
-            System.out.println(res.name());
-        }
-
+        
+        return resultats;
     }
 }
